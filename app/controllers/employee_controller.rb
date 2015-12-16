588d80d2ed98919ad622c589293b0776078026eb
Alex <alexandralaudon@gmail.com>
2015-12-15 17:25:22 -0700

    added all_tables method to iterate through tables

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,5 +1,15 @@
 class EmployeeController < ApplicationController
+
+  helper_method :all_tables
+
   def index
-    employees = Employee.all
+    @employees = Employee.all
   end
+
+  def all_tables
+    (ActiveRecord::Base.connection.tables - ['schema_migrations']).each do |table|
+      table
+    end
+  end
+
 end
