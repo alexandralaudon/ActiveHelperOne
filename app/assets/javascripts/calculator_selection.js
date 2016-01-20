18de01338a03152bab081ae9734bfe63c7eb00db
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2016-01-19 19:28:48 -0700

    WIP: Changed HTML to include div that aids in jQuery selection, debugged jQuery selectors

---
+++ app/assets/javascripts/calculator_selection.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  console.log("Document is ready!");
+  $(".tables").on("click", "p", function() {
+    var tableName = $(this).parent().find('h1').get(0).innerText;
+    var columnName = $(this).text();
+    console.log($(this));
+    console.log("Clicked on a column name: " + columnName);
+    console.log(tableName);
+    console.log("Closest table name: " + tableName);
+    
+  });
+});
